edd7ab2ffe418df65b7c806be811be577083d16e
Kamie Short <kamie.short@gmail.com>
2022-02-02 16:24:20 -0800

    plan complete, HTML complete, DOM elements, let state, event listener for sumbit new form complete

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-## Goblin Fighter Plan
+## Wizards vs. Dragons Plan
+
+Styles: Harry Potter themed.
+get Hogwarts background.
+You are wizard, computer is dragon.
+
+instead of HP use spells?
+
+## Order of Operations
+
+HTML
+add HMTL labels, span -Wizard HP, form with buttons,
+-set up div class for dragons (will apend later)
+set up ID's for all
+--CSS base structure as we go.
+APP.js
+grab dom elements -get element by ID
+
+let state
+set up objects for each dragon
+
+add event listener for submit
+prevent default
+make new object with goblin name and add math random to create likleness of hit
+
+set up renderDragon function in utils.js
--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
+import { renderDragon } from './utils.js';
+const defeatedNumberEl = document.getElementById('defeated-number')
+const spellsRemainingEl = document.getElementById('wizard-spells')
 
 // let state
+let spells = 5;
+let defeatedDragons = 0;
+let dragons = [
+  {name: 'Fireball', hp: 5},
+  {name: 'Scales', hp: 5},
+  (name: 'Patty', hp: 5},
+];
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+//new dragon form
+form.addEventListener('submit', (e)) => {
+  e.preventDefault();
+  
+}
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
